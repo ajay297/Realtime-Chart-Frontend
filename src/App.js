f7482185df1bd1194ade286fcb9cd57a6a8a2453
ajay297 <dangilajay9@gmail.com>
2021-04-20 23:38:27 +0530

    pushing into netlify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+import {
+  BarChart,
+  Bar,
+  Line,
+  LineChart,
+  XAxis,
+  YAxis,
+  Tooltip
+} from 'recharts';
 
+const ENDPOINT = 'https://realtime-chart-1.herokuapp.com/';
+
+const connectionOptions = {
+  "force new connection": true,
+  "reconnectionAttempts": "Infinity",
+  "timeout": 10000,
+  "transports": ["websocket"]
+};
 function App() {
+  const [response, setResponse] = useState([]);
+
+  useEffect(() => {
+    console.log("hello");
+    const socket = socketIOClient(ENDPOINT, connectionOptions);
+    socket.on('FromAPI', data => {
+      setResponse(response => [...response, data]);
+    });
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bitcoin</h1>
+      <LineChart width={500} height={300} data={response}>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Line dataKey="doge" stroke="#8884d8" />
+        <Line dataKey="btc" stroke="#82ca9d" />
+
+      </LineChart>
     </div>
   );
 }
